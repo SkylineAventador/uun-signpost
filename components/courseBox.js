40477cd14222ffdb653071bb7f6743750e5c9152
Dmytro Kravtsov <dmytro.kravtsov@unicorn.com>
2021-05-28 00:43:46 +0200

    Grid - image cards with courses - json data connected and fetched.

--- components/courseBox.js
+++ components/courseBox.js
@@ -1,12 +1,17 @@
 import {Box, Flex, Link} from "@chakra-ui/react";
+import LinkNext from 'next/link'
 
+// bgImage={"url(https://picsum.photos/640/480)"}
 export default function CourseBox(props){
     return(
-        <Box position={"relative"} bgImage={"url(https://picsum.photos/640/480)"} boxShadow={"2xl"}
+        <Box position={"relative"} bgImage="/code_background.jpg" boxShadow={"2xl"}
              backgroundSize={"cover"} backgroundPosition={"center"} backgroundRepeat={"no-repeat"} borderRadius={"xl"}>
             <Flex direction={"column"} position={"absolute"} background={"rgba(0,0,0,0.5)"} w={"100%"}
                   h={"100%"} justify={"center"} align={"center"} textAlign={"center"} borderRadius={"xl"} p={2}>
-                <Link color={"white"} fontSize={[".5rem","1rem","2rem"]} href={"https://google.cz"} isExternal>Architektura cloudovych aplikaci</Link>
+                <LinkNext href="https://google.cz" passHref>
+                    <Link color={"white"} fontSize={["1rem", "1.5rem", "2rem"]} isExternal
+                          noOfLines={5}>Architektura cloudovych aplikaci</Link>
+                </LinkNext>
             </Flex>
         </Box>
     )
