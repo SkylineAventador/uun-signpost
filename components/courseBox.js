30716b6a6eb96e88862a66740eeda1ecbc3ccc5f
Dmytro Kravtsov <dmytro.kravtsov@plus4u.net>
2021-08-25 08:42:42 +0200

    Project renewal. Initial minor changes.

--- components/courseBox.js
+++ components/courseBox.js
@@ -1,10 +1,11 @@
 import {Box, Flex, Link} from "@chakra-ui/react";
 import LinkNext from 'next/link'
 
-// bgImage={"url(https://picsum.photos/640/480)"}
+//Each box must contain background image from recourses.
+// bgImage={"url(https://picsum.photos/640/480)"} - random img for test purposes.
 export default function CourseBox(props){
     return(
-        <Box position={"relative"} bgImage="/code_background.jpg" boxShadow={"2xl"}
+        <Box position={"relative"} boxShadow={"2xl"}
              backgroundSize={"cover"} backgroundPosition={"center"} backgroundRepeat={"no-repeat"} borderRadius={"xl"}>
             <Flex direction={"column"} position={"absolute"} background={"rgba(0,0,0,0.5)"} w={"100%"}
                   h={"100%"} justify={"center"} align={"center"} textAlign={"center"} borderRadius={"xl"} p={2}>
