1e1a4ed6fff17cedd2d83c03a8df42d4d31d1e7c
Dmytro Kravtsov <dmytro.kravtsov@plus4u.net>
2021-08-27 19:53:47 +0200

    Test courses from json connected.

--- components/courseBox.js
+++ components/courseBox.js
@@ -1,19 +1,22 @@
-import {Box, Flex, Link} from "@chakra-ui/react";
+import {Box, Flex, Link, Text, GridItem} from "@chakra-ui/react";
 import LinkNext from 'next/link'
 
 //Each box must contain background image from recourses.
 // bgImage={"url(https://picsum.photos/640/480)"} - random img for test purposes.
 export default function CourseBox(props){
     return(
-        <Box position={"relative"} boxShadow={"2xl"}
+        <GridItem position={"relative"} boxShadow={"2xl"}
              backgroundSize={"cover"} backgroundPosition={"center"} backgroundRepeat={"no-repeat"} borderRadius={"xl"}>
-            <Flex direction={"column"} position={"absolute"} background={"rgba(0,0,0,0.5)"} w={"100%"}
+            <Flex direction={"column"} position={"absolute"} background={"pink.50"} w={"100%"}
                   h={"100%"} justify={"center"} align={"center"} textAlign={"center"} borderRadius={"xl"} p={2}>
-                <LinkNext href={props.link} passHref>
-                    <Link color={"white"} fontSize={["1rem", "1.5rem", "2rem"]} isExternal
+                <LinkNext href="/" passHref>
+                    <Link color={"black"} fontSize={["1rem", "1.5rem", "2rem"]} isExternal
                           noOfLines={5}>{props.name}</Link>
                 </LinkNext>
             </Flex>
-        </Box>
+        </GridItem>
+      // <Box bg={"tomato"}>
+      //   <Text>{props.name}</Text>
+      // </Box>
     )
 }
