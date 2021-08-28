82d4e02ba58a12c92cae3fb36704771f0fb965e0
Dmytro Kravtsov <dmytro.kravtsov@plus4u.net>
2021-08-28 15:54:50 +0200

    Savepoint, home page cards func created.

--- components/courseBox.js
+++ components/courseBox.js
@@ -1,22 +1,15 @@
-import {Box, Flex, Link, Text, GridItem} from "@chakra-ui/react";
+import {Center, Link} from "@chakra-ui/react";
 import LinkNext from 'next/link'
 
 //Each box must contain background image from recourses.
 // bgImage={"url(https://picsum.photos/640/480)"} - random img for test purposes.
-export default function CourseBox(props){
-    return(
-        <GridItem position={"relative"} boxShadow={"2xl"}
-             backgroundSize={"cover"} backgroundPosition={"center"} backgroundRepeat={"no-repeat"} borderRadius={"xl"} border={"solid"}>
-            <Flex direction={"column"} position={"absolute"} background={"yellow.50"} w={"100%"}
-                  h={"100%"} justify={"center"} align={"center"} textAlign={"center"} borderRadius={"xl"} p={2}>
-                <LinkNext href="/" passHref>
-                    <Link color={"black"} fontSize={["1rem", "1.5rem", "2rem"]} isExternal
-                          noOfLines={5}>{props.name}</Link>
-                </LinkNext>
-            </Flex>
-        </GridItem>
-      // <Box bg={"tomato"}>
-      //   <Text>{props.name}</Text>
-      // </Box>
-    )
+export default function CourseBox(props) {
+  return (
+    <Center border={"solid"} borderRadius={"xl"} bg={props.bgColor || "yellow.50"} w={"100%"} h={"100%"}>
+      <LinkNext href={props.link} passHref>
+        <Link color={"black"} fontSize={props.fontSize || "2rem"} isExternal={props.external || false}
+              noOfLines={5} placeContent={"center"}>{props.name}</Link>
+      </LinkNext>
+    </Center>
+  )
 }
